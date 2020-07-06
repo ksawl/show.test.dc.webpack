b12ac98c24b07b56fb6751fd249e433d06341066
Serhii Karavashkin <master@karaone.info>
2020-07-06 21:36:07 +0300

    start old page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import "./js/common";
+import "./js/main";
 import "./assets/css/style.css";
+import "./assets/css/media.css";
+import "./assets/css/media_768.css";
+import "./assets/css/media_992.css";
+import "./assets/css/media_1200.css";
 import "./assets/scss/main.scss";
 
 //import Vue from 'vue'
--- src/js/common.js
+++
@@ -1,2 +0,0 @@
-let add = (a, b) => a + b;
-console.log("Fn add() ", add(2, 16));
